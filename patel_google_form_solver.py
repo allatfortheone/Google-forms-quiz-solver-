692c7ecd892bc7c2a31c4bdc5e02fddf83c30674
allatfortheone <siddharthkaranala3@gmail.com>
2025-03-12 12:38:15 -0400

    Update patel_google_form_solver.py

--- patel_google_form_solver.py
+++ patel_google_form_solver.py
@@ -6,6 +6,14 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
 # Function to print the PATEL banner
 def print_banner():
     ascii_art = pyfiglet.figlet_format("PATEL", font="slant")
